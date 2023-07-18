49be01f82119da57ab4689a7be84530d5952902a
xhzz <1789010493@qq.com>
2023-07-18 10:06:06 +0800

    实体类

---
+++ src/main/java/com/iweb/App.java
@@ -0,0 +1,16 @@
+package com.iweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main(String[] args) {
+        String b="MyString";
+        String c="My"+"String";
+        System.out.println(b==c);
+
+    }
+}
