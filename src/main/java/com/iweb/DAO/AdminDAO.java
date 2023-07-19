1918230dc80198103e0eb6478c39cfed0204f423
xhzz <1789010493@qq.com>
2023-07-19 21:14:45 +0800

    主界面 AdminDAO及AdminDAOimpl conteoller userService adminService

--- src/main/java/com/iweb/DAO/AdminDAO.java
+++ src/main/java/com/iweb/DAO/AdminDAO.java
@@ -1,6 +1,8 @@
 package com.iweb.DAO;
 
 import com.iweb.clazzs.Admin;
+import com.iweb.clazzs.Order;
+import com.iweb.clazzs.Product;
 
 import java.util.Collection;
 
@@ -22,6 +24,36 @@ public interface AdminDAO {
      * @return 返回所有管理员集合
      */
     Collection<Admin> listAllAdmin();
+
+    /**增加商品信息
+     * @param product 传入商品对象
+     */
+    void addProduct(Product product);
+
+    /**删除商品信息
+     * @param pid 要删除的商品id
+     */
+    void  deleteProduct(int pid);
+
+    /**修改商品信息
+     * @param pid 修改的商品的id
+     */
+    void changeProduct(int pid);
+
+    /**查看所有商品
+     * @return 返回商品集合
+     */
+    Collection<Product> checkProduct();
+
+    /**查看订单信息
+     * @return 返回订单集合
+     */
+    Collection<Order> checkOrder();
+
+    /**修改订单信息
+     * @param oid 要修改订单的id
+     */
+    void changeOrder(int oid);
 
 
 }
