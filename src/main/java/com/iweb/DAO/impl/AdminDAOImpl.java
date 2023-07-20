27e7b62f1572ce816d8c746cb1102f0571f26723
tang <2576208723@qq.com>
2023-07-20 08:29:25 +0800

    excle 导出商品

--- src/main/java/com/iweb/DAO/impl/AdminDAOImpl.java
+++ src/main/java/com/iweb/DAO/impl/AdminDAOImpl.java
@@ -3,6 +3,10 @@ package com.iweb.DAO.impl;
 import com.iweb.DAO.AdminDAO;
 import com.iweb.Until.DBUtil;
 import com.iweb.clazzs.Admin;
+import com.iweb.clazzs.Order;
+import com.iweb.clazzs.Product;
+import com.iweb.view.View;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,6 +20,7 @@ import java.util.List;
  */
 public class AdminDAOImpl implements AdminDAO {
     Connection connection;
+
     {
         try {
             connection = DBUtil.getConnection();
@@ -35,7 +40,7 @@ public class AdminDAOImpl implements AdminDAO {
         }
         //        连接数据库 进行数据插入
         try (
-             PreparedStatement ps = connection.prepareStatement(sql)) {
+                PreparedStatement ps = connection.prepareStatement(sql)) {
             //            为每个?设置相应的值
             ps.setString(1, admin.getAdmin_name());
             ps.setString(2, admin.getAdmin_password());
@@ -52,7 +57,7 @@ public class AdminDAOImpl implements AdminDAO {
         List<Admin> list = new ArrayList<>();
         String sql = "select * from admin";
         try (
-             PreparedStatement ps = connection.prepareStatement(sql)
+                PreparedStatement ps = connection.prepareStatement(sql)
         ) {
 //            sql语句的执行结果存入到结果集中
             ResultSet rs = ps.executeQuery();
@@ -69,5 +74,188 @@ public class AdminDAOImpl implements AdminDAO {
         }
 //        判断集和是否为空 做空值处理
         return list.isEmpty() ? null : list;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+
+        String sql = "select product_name from product";
+        String sql1 = "INSERT INTO product(product_name,product_price,stock_num,sales_num,property_id,admin_id)" +
+                " VALUES(?,?,?,?,?,?)";
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql);
+             PreparedStatement ps1 = c.prepareStatement(sql1);
+        ) {
+            ResultSet rs = ps.executeQuery();
+            boolean isExist = true;
+            while (rs.next()) {
+                if (product.getProduct_name().equals(rs.getString(1))) {
+                    System.out.println("商品已经存在");
+                    isExist = false;
+                    break;
+                }
+            }
+            if (isExist) {
+                ps1.setString(1, product.getProduct_name());
+                ps1.setDouble(2, product.getProduct_price());
+                ps1.setInt(3, product.getStock_num());
+                ps1.setInt(4, product.getSales_num());
+                ps1.setInt(5, product.getProperty_id());
+                ps1.setInt(6, product.getAdmin_id());
+                ps1.execute();
+                System.out.println("添加成功");
+            }
+            View.adminLoginSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteProduct(int pid) {
+        String sql = "select product_id from product";
+        String sql1 = "delete from product where product_id=" + pid;
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql);
+             PreparedStatement ps1 = c.prepareStatement(sql1)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            boolean isExist = true;
+            while (rs.next()) {
+                if (pid == rs.getInt(1)) {
+                    ps1.execute();
+                    isExist = false;
+                    System.out.println("删除成功");
+                    break;
+                }
+            }
+            if (isExist) {
+                System.out.println("商品不存在");
+            }
+            View.adminLoginSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void changeProduct(int pid, double pr, int st, int sa) {
+        String sql = "select product_id from product";
+        String sql1 = "update product set product_price=?,stock_num=?,sales_num=? where product_id=" + pid;
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql);
+             PreparedStatement ps1 = c.prepareStatement(sql1)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            boolean isExist = true;
+            while (rs.next()) {
+                if (pid == rs.getInt(1)) {
+                    ps1.setDouble(1, pr);
+                    ps1.setInt(2, st);
+                    ps1.setInt(3, sa);
+                    ps1.execute();
+                    System.out.println("修改成功");
+                    isExist = false;
+                    break;
+                }
+            }
+            if (isExist) {
+                System.out.println("商品不存在");
+            }
+            View.adminLoginSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Collection<Product> checkProduct() {
+        String sql = "SELECT product_id,product_name,product_price,stock_num,sales_num" +
+                ",product.property_id,property_name,property_describe,admin_id FROM product,property order by sales_num desc limit 0,10";
+        List<Product> list = new ArrayList<>();
+        try {
+            Connection c = DBUtil.getConnection();
+            PreparedStatement ps = c.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Product product = new Product();
+                product.setProduct_id(rs.getInt(1));
+                product.setProduct_name(rs.getString(2));
+                product.setProduct_price(rs.getDouble(3));
+                product.setStock_num(rs.getInt(4));
+                product.setSales_num(rs.getInt(5));
+                product.setProperty_id(rs.getInt(6));
+                product.setProperty_name(rs.getString(7));
+                product.setProperty_describe(rs.getString(8));
+                product.setAdmin_id(rs.getInt(9));
+                list.add(product);
+            }
+            for (Product p : list
+            ) {
+                System.out.println(p);
+            }
+            View.adminLoginSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list.isEmpty() ? null : list;
+    }
+
+    @Override
+    public Collection<Order> checkOrder() {
+        String sql = "select * from orders";
+        List<Order> list = new ArrayList<>();
+        try {
+            Connection c = DBUtil.getConnection();
+            PreparedStatement ps = c.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Order order = new Order();
+                order.setOrder_num(rs.getInt(1));
+                order.setProduct_id(rs.getInt(3));
+                order.setProduct_name(rs.getString(4));
+                order.setProduct_price(rs.getDouble(5));
+                order.setProduct_num(rs.getInt(6));
+                order.setOrder_state(rs.getString(7));
+                list.add(order);
+            }
+            for (Order o : list
+            ) {
+                System.out.println(o);
+            }
+            View.adminLoginSuccess();
+        } catch (Exception e) {
+
+        }
+        return list.isEmpty() ? null : list;
+    }
+
+    @Override
+    public void changeOrder(int oid, String state) {
+        String sql = "select order_num from orders ";
+        String sql1 = "update orders set order_state=? where order_num=?" ;
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql);
+             PreparedStatement ps1 = c.prepareStatement(sql1)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            boolean isExist = true;
+            while (rs.next()) {
+                if (oid==rs.getInt(1)) {
+                    ps1.setString(1,state);
+                    ps1.setInt(2,oid);
+                    ps1.execute();
+                    System.out.println("修改成功");
+                    isExist=false;
+                    break;
+                }
+            }
+            if (isExist){
+                System.out.println("订单不存在");
+            }
+            View.adminLoginSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
